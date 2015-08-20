da8ce873bf64f2e9d74b8f136ce316b5faa3db14
Marie-Odile Del Carlo <marie-odiledelcarlo@MacBook-Pro-de-Marie-Odile-Del-Carlo-2.local>
2015-08-20 14:09:12 +0200

    change date

--- LimaMockup/DetailViewController.m
+++ LimaMockup/DetailViewController.m
@@ -2,7 +2,7 @@
 //  DetailViewController.m
 //  LimaMockup
 //
-//  Created by Marie-Odile Del Carlo on 17/08/2015.
+//  Created by Marie-Odile Del Carlo on 14/08/2015.
 //  Copyright (c) 2015 Marie-Odile Del Carlo. All rights reserved.
 //
 
